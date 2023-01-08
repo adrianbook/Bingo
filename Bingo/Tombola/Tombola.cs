5e274ae19fe349d8fabf2ebd4bd79f6c3b900f10
adrianbook <adrian@exsolve.se>
2023-01-08 22:36:03 +0100

    add tombola class
    class that creates a randomized stack of numbers 1 through 75
    and returns them one by one
    
    appropriate tests

---
+++ Bingo/Tombola/Tombola.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TombolaProject;
+using TombolaProject.Extensions;
+
+namespace TombolaProject
+{
+    public class Tombola
+    {
+        private Stack<int> Numbers;
+        
+        public Tombola()
+        {
+            var shuffledNumbers = Enumerable.Range(1, 75).ToList().ShuffleAndReturn();
+            Numbers = new Stack<int>(shuffledNumbers);
+        }
+        public int NextNumber()
+        {
+            return Numbers.Pop();
+        }
+
+        
+    }
+
+}
--- Bingo/TombolaTest/UnitTest1.cs
+++ Bingo/TombolaTest/UnitTest1.cs
@@ -1,11 +1,32 @@
+using System.Security.Cryptography.X509Certificates;
+using TombolaProject;
+
 namespace TombolaTest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestNumberProduced()
         {
+            Tombola t = new Tombola();
+            int num = t.NextNumber();
+
+            Assert.IsTrue(num < 76);
+            Assert.IsTrue(num > 0);
+        }
+
+        [TestMethod]
+        public void TestNumbersNotInSequence()
+        {
+            Tombola t = new Tombola();
+            for (int i = 75; i > 60; i--)
+            {
+                int n = t.NextNumber();
+                Console.WriteLine(n);
+                if (n != i) break;
+                if (i == 61) Assert.Fail();
+            }
         }
     }
 }
